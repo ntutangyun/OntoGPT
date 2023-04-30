3c102899b88d81c5455c89e4f88f353fbc666e60
Yun Tang <yun.tang@warwick.ac.uk>
2023-04-30 14:52:06 +0100

    UI update. concept hierarchy extraction task in progress

--- src/ContentComponent.js
+++ src/ContentComponent.js
@@ -1,12 +1,17 @@
 // this is the content component
 
-import React, {useEffect} from "react";
-import {Col, Layout, Row,} from "antd";
+import React, {useEffect, useState} from "react";
+import {Button, Col, Layout, message, Row, Tabs,} from "antd";
 
 import * as d3 from "d3";
 import * as d3Graphviz from "d3-graphviz";
 
 import {Input} from "antd";
+import {
+    ConceptHierarchyExtractionFormatTemplate,
+    ConceptHierarchyExtractionInstructionTemplate,
+    DomainContextTemplate
+} from "./PromptTemplates";
 
 const {TextArea} = Input;
 const {Content} = Layout;
@@ -17,26 +22,44 @@ const contentStyle = {
 
 const dotEditorStyle = {
     fontFamily: "monospace",
-    height: "calc(100vh - 200px)",
+    height: "calc(100vh - 220px)",
 };
 
 const graphStyle = {
     border: "1px solid #ddd",
     backgroundColor: "white",
-    marginLeft: "auto",
-    marginRight: "auto",
-    maxHeight: "calc(100vh - 200px)",
-    maxWidth: "90%",
+    maxHeight: "calc(100vh - 220px)",
+    maxWidth: "100%",
     overflowX: "hidden",
-    overflowY: "hidden"
+    overflowY: "hidden",
+    textAlign: "left",
 };
 
-const visualizationColStyle = {
-    textAlign: "center",
+const colStyle = {
+    padding: "0 1rem 1rem 1rem",
+    textAlign: "center"
+};
+
+const CommonTextAreaStyle = {
+    fontFamily: "monospace",
+    height: "calc(100vh - 280px)",
+};
+
+const generatedPromptStyle = {
+    fontFamily: "monospace",
+    height: "calc(100vh - 280px)",
+    textAlign: "left",
 };
 
 export default function ContentComponent() {
-    const [dotString, setDotString] = React.useState("digraph {a -> b}");
+    const [dotString, setDotString] = useState("digraph {\n    a -> b;\n}");
+    const [domainContextInput, setDomainContextInput] = useState(DomainContextTemplate);
+    const [conceptHierarchyExtractionInstructionInput, setConceptHierarchyExtractionInstructionInput] =
+        useState(ConceptHierarchyExtractionInstructionTemplate);
+    const [conceptHierarchyExtractionFormatInput, setConceptHierarchyExtractionFormatInput] =
+        useState(ConceptHierarchyExtractionFormatTemplate);
+    const [generatedPrompt, setGeneratedPrompt] = useState("");
+
     useEffect(() => {
         try {
             d3.select("#graph").graphviz({fit: true})
@@ -47,23 +70,100 @@ export default function ContentComponent() {
 
     }, [dotString]);
 
-    const onTextAreaChange = (e) => {
-        setDotString(e.target.value);
+    useEffect(() => {
+        setGeneratedPrompt(domainContextInput + conceptHierarchyExtractionInstructionInput + conceptHierarchyExtractionFormatInput);
+    }, [domainContextInput,
+        conceptHierarchyExtractionInstructionInput,
+        conceptHierarchyExtractionFormatInput
+    ]);
+
+    const onCopyPromptGenerated = () => {
+        const textarea = document.getElementById("textarea-prompt-generated");
+        textarea.select();
+        document.execCommand("copy");
+        message.success("Copied");
     };
+
+    const promptEngineeringTabs = [
+        {
+            key: "context",
+            label: `Context`,
+            children: <TextArea style={CommonTextAreaStyle} value={domainContextInput}
+                                onChange={e => setDomainContextInput(e.target.value)}/>,
+        },
+        {
+            key: "instruction",
+            label: `Instruction`,
+            children: <TextArea style={CommonTextAreaStyle}
+                                value={conceptHierarchyExtractionInstructionInput}
+                                onChange={e => setConceptHierarchyExtractionInstructionInput(e.target.value)}/>,
+        },
+        {
+            key: "format",
+            label: `Format`,
+            children: <TextArea style={CommonTextAreaStyle} value={conceptHierarchyExtractionFormatInput}
+                                onChange={e => setConceptHierarchyExtractionFormatInput(e.target.value)}/>,
+        },
+        {
+            key: "prompt",
+            label: `Prompt`,
+            children: <div>
+                <TextArea style={CommonTextAreaStyle}
+                          readOnly={true}
+                          value={generatedPrompt}
+                          id={"textarea-prompt-generated"}>
+                </TextArea>
+            </div>
+        }
+    ];
+
+    const executionTabs = [
+        {
+            key: "controls",
+            label: `Controls`,
+            children: <div style={{textAlign: "left"}}>
+                <Row style={{alignItems: "center"}}>
+                    <Col>
+                        <Button onClick={onCopyPromptGenerated}>Copy & Execute</Button>
+                    </Col>
+                    <Col>
+                        <div style={{paddingLeft: "0.5rem"}}>Click to copy the prompt. Paste it in a new ChatGPT session
+                            to execute it.
+                        </div>
+                    </Col>
+                </Row>
+
+            </div>
+        },
+        {
+            key: "log",
+            label: `Log`,
+            children: <TextArea style={CommonTextAreaStyle} value={dotString}/>
+        },
+        {
+            key: "dot",
+            label: `Hierarchy`,
+            children: <TextArea
+                style={CommonTextAreaStyle}
+                value={dotString}
+                placeholder="input the DOT content"
+                onChange={e => setDotString(e.target.value)}/>
+        }
+    ];
+
     return (
         <Content style={contentStyle}>
             <Row>
-                <Col span={9}>
-                    <h1>Controls</h1>
+                <Col span={9} style={colStyle}>
+                    <h1>Prompt Engineering</h1>
+                    <Tabs defaultActiveKey="context" items={promptEngineeringTabs}/>
                 </Col>
-                <Col span={6}>
-                    <h1 style={{textAlign: "center"}}>DOT Editor</h1>
-                    <TextArea
-                        style={dotEditorStyle}
-                        placeholder="input the DOT content"
-                        onChange={onTextAreaChange}/>
+                <Col span={6} style={colStyle}>
+                    <h1 style={{textAlign: "center"}}>Execution</h1>
+                    <Tabs defaultActiveKey={"controls"} items={executionTabs}/>
+
                 </Col>
-                <Col span={9} style={visualizationColStyle}>
+                <Col span={9} style={colStyle}>
                     <h1>Visualisation</h1>
                     <div id="graph" style={graphStyle}/>
                 </Col>
